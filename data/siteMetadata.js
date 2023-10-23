53179f5ecf82049ce6a7c85203f8dd735fbd60c0
XuYi-fei <857998989@qq.com>
2023-10-23 20:43:02 +0800

    refactor: Refactor footer

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,12 +11,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  // email: 'address@yoursite.com',
+  // github: 'https://github.com',
+  // twitter: 'https://twitter.com/Twitter',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  // linkedin: 'https://www.linkedin.com',
   locale: 'zh-CN',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
